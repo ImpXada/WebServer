6c6a8f5b852e6a5c82eae36287ac97ffec8b5e5a
ImpXada <kongfanshu0111@outlook.com>
2022-04-19 15:43:54 +0800

    sync

--- htmlFSM.cpp
+++ htmlFSM.cpp
@@ -8,7 +8,10 @@
 #include <errno.h>
 #include <string.h>
 #include <fcntl.h>
+#include <iostream>
 #define BUFFER_SIZE 4096
+
+using namespace std;
 enum CHECK_STATE
 {
     CHECK_STATE_REQUESTLINE = 0,
@@ -16,18 +19,18 @@ enum CHECK_STATE
 };
 enum LINE_STATUS //从状态机，解析一行
 {
-    LINE_OK = 0,//
+    LINE_OK = 0, //
     LINE_BAD,
     LINE_OPEN
 };
 enum HTTP_CODE
 {
-    NO_REQUEST,//请求不完整
-    GET_REQUEST,//完整的用户请求
-    BAD_REQUEST,//请求语法错误
-    FORBIDDEN_REQUEST,//客户对资源无访问权限
-    INTERNAL_ERROR,//服务器内部错误
-    CLOSE_REQUEST//关闭连接
+    NO_REQUEST,        //请求不完整
+    GET_REQUEST,       //完整的用户请求
+    BAD_REQUEST,       //请求语法错误
+    FORBIDDEN_REQUEST, //客户对资源无访问权限
+    INTERNAL_ERROR,    //服务器内部错误
+    CLOSE_REQUEST      //关闭连接
 };
 
 static const char *szret[] = {"I get a correct result\n", "Something wrong\n"};
@@ -35,21 +38,213 @@ static const char *szret[] = {"I get a correct result\n", "Something wrong\n"};
 LINE_STATUS parse_line(char *buffer, int &checked_index, int &read_index)
 {
     char temp;
-    for (; checked_index < read_index;++checked_index)
+    for (; checked_index < read_index; ++checked_index)
     {
         temp = buffer[checked_index];
-        if(temp=='\r')
+        if (temp == '\r')
         {
-            if((checked_index+1)==read_index)
+            if ((checked_index + 1) == read_index)
             {
                 return LINE_OPEN;
             }
-            else if (buffer[checked_index+1]=='\n')
+            else if (buffer[checked_index + 1] == '\n')
             {
-                buffer[checked_index++]='\0';
-                buffer[checked_index++]='\0';
+                buffer[checked_index++] = '\0';
+                buffer[checked_index++] = '\0';
                 return LINE_OK;
             }
+            return LINE_BAD;
+        }
+        else if (temp == '\n')
+        {
+            if ((checked_index > 1) && buffer[checked_index - 1] == '\r')
+            {
+                buffer[checked_index - 1] = '\0';
+                buffer[checked_index++] = '\0';
+                return LINE_OK;
+            }
+            return LINE_BAD;
         }
     }
+    return LINE_OPEN;
+}
+
+HTTP_CODE parse_requestline(char *temp, CHECK_STATE &checkstate)
+{
+    char *url = strpbrk(temp, " \t");
+    if (!url)
+    {
+        return BAD_REQUEST;
+    }
+    *url++ = '\0';
+    char *method = temp;
+    if (strcasecmp(method, "GET") == 0)
+    {
+        cout << "The request method is GET" << endl;
+    }
+    else
+    {
+        return BAD_REQUEST;
+    }
+
+    url += strspn(url, " \t");
+    char *version = strpbrk(url, " \t");
+
+    if (!version)
+    {
+        return BAD_REQUEST;
+    }
+    if (strcasecmp(version, "HTTP/1.1") != 0)
+    {
+        return BAD_REQUEST;
+    }
+    if (strncasecmp(url, "http://", 7) == 0)
+    {
+        url += 7;
+        url = strchr(url, '/');
+    }
+    if (!url || url[0] != '/')
+    {
+        return BAD_REQUEST;
+    }
+    cout << "The request URL is: " << url << endl;
+    checkstate = CHECK_STATE_HEADER;
+    return NO_REQUEST;
+}
+
+HTTP_CODE parse_headers(char *temp)
+{
+    if (temp[0] == '\0')
+    {
+        return GET_REQUEST;
+    }
+    else if (strncasecmp(temp, "Host:", 5) == 0)
+    {
+        temp += 5;
+        temp += strspn(temp, " \t");
+        cout << "the request host is: " << temp << endl;
+    }
+    else
+    {
+        cout << "I cannot handle this header" << endl;
+    }
+    return NO_REQUEST;
+}
+
+HTTP_CODE parse_content(char *buffer, int &checked_index, CHECK_STATE &checkstate, int &read_index, int &start_line)
+{
+    LINE_STATUS linestatus = LINE_OK;
+    HTTP_CODE retcode = NO_REQUEST;
+    while ((linestatus = parse_line(buffer, checked_index, read_index)) == LINE_OK)
+    {
+        char *temp = buffer + start_line;
+        start_line = checked_index;
+
+        switch (checkstate)
+        {
+        case CHECK_STATE_REQUESTLINE:
+            retcode = parse_requestline(temp, checkstate);
+            if (retcode == BAD_REQUEST)
+            {
+                return BAD_REQUEST;
+            }
+            break;
+        case CHECK_STATE_HEADER:
+        {
+            retcode = parse_headers(temp);
+            if (retcode == BAD_REQUEST)
+            {
+                return BAD_REQUEST;
+            }
+            else if (retcode == GET_REQUEST)
+            {
+                return GET_REQUEST;
+            }
+            break;
+        }
+        default:
+        {
+            return INTERNAL_ERROR;
+            break;
+        }
+        }
+    }
+    if (linestatus == LINE_OPEN)
+    {
+        return NO_REQUEST;
+    }
+    else
+    {
+        return BAD_REQUEST;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 2)
+    {
+        return 1;
+    }
+    const char *ip = argv[1];
+    int port = atoi(argv[2]);
+
+    struct sockaddr_in address;
+    bzero(&address, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(ip);
+    address.sin_port = htons(port);
+
+    int listenfd = socket(PF_INET, SOCK_STREAM, 0);
+    assert(listenfd >= 0);
+    int ret = bind(listenfd, (struct sockaddr *)&address, sizeof(address));
+    assert(ret != -1);
+    ret = listen(listenfd, 5);
+    assert(ret != -1);
+    struct sockaddr_in client_address;
+    socklen_t client_addrlength = sizeof(client_address);
+    int fd = accept(listenfd, (struct sockaddr *)&client_address, &client_addrlength);
+
+    if (fd < 0)
+    {
+        cout << errno << endl;
+    }
+    else
+    {
+        char buffer[BUFFER_SIZE];
+        memset(buffer, '\0', BUFFER_SIZE);
+        int data_read = 0;
+        int read_index = 0;
+        int checked_index = 0;
+        int start_line = 0;
+        CHECK_STATE checkstate = CHECK_STATE_REQUESTLINE;
+        while (1)
+        {
+            data_read = recv(fd, buffer + read_index, BUFFER_SIZE - read_index, 0);
+            if (data_read == -1)
+            {
+                cout << "reading failed" << endl;
+                break;
+            }
+
+            else if (data_read == 0)
+            {
+                cout << "remote client has closed the connection" << endl;
+                break;
+            }
+            read_index += data_read;
+            HTTP_CODE result = parse_content(buffer, checked_index, checkstate, read_index, start_line);
+            if (result == NO_REQUEST)
+            {
+                continue;
+            }
+            else if (result == GET_REQUEST)
+            {
+                send(fd, szret[0], strlen(szret[0]), 0);
+                break;
+            }
+            close(fd);
+        }
+    }
+    close(listenfd);
+    return 0;
 }
