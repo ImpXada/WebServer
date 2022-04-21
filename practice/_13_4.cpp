6001bca1d551efa6688ffeccb0086ea0d2d6b428
ImpXada <kongfanshu0111@outlook.com>
2022-04-21 12:30:11 +0800

    locker.h

---
+++ practice/_13_4.cpp
@@ -0,0 +1,66 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/epoll.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define USER_LIMIT 5
+#define BUFFER_SIZE 1024
+#define FD_LIMIT 65535
+#define MAX_EVENT_NUMBER 1024
+#define PROCESS_LIMIT 65536
+
+struct client_data
+{
+    sockaddr_in address;
+    int connfd;
+    pid_t pid;
+    int pipefd[2];
+};
+
+static const char *shm_name = "/my_shm";
+int sig_pipefd[2];
+int epollfd;
+int listenfd;
+int shmfd;
+char *share_mem = 0;
+client_data *users = 0;
+int *sub_process = 0;
+int user_count = 0;
+bool stop_child = false;
+
+int setnonblocking(int fd)
+{
+    int old_option = fcntl(fd, F_GETFL);
+    int new_option = old_option | O_NONBLOCK;
+    fcntl(fd, F_SETFL, new_option);
+    return old_option;
+}
+
+void addfd(int epollfd, int fd)
+{
+    epoll_event event;
+    event.data.fd = fd;
+    event.events = EPOLLIN | EPOLLET;
+    epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+    setnonblocking(fd);
+}
+
+void sig_handler(int sig)
+{
+    int save_errno = errno;
+    int msg = sig;
+    send(sig_pipefd[1], (char *)&msg, 1, 0);
+    errno = save_errno;
+}
